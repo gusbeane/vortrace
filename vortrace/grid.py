e5a123f78241a3f297e9ce48d0508e4d6682b4f6
Gus Beane <angus.beane@cfa.harvard.edu>
2022-06-10 18:11:22 -0400

    Run yapf on code

--- vortrace/grid.py
+++ vortrace/grid.py
@@ -1,9 +1,10 @@
 import numpy as np
 from numba import njit
 
+
 @njit
 def _generate_base_grid(extent, nres):
-    grid = np.zeros((nres[0]*nres[1], 3))
+    grid = np.zeros((nres[0] * nres[1], 3))
 
     deltax = (extent[0][1] - extent[0][0]) / (nres[0])
     deltay = (extent[1][1] - extent[1][0]) / (nres[1])
@@ -11,7 +12,7 @@ def _generate_base_grid(extent, nres):
     # Now create grid.
     for i in range(nres[0]):
         for j in range(nres[1]):
-            k = i*nres[1] + j
+            k = i * nres[1] + j
 
             x = deltax * (i + 0.5)
             y = deltay * (j + 0.5)
@@ -20,6 +21,7 @@ def _generate_base_grid(extent, nres):
             grid[k][1] = y
 
     return grid
+
 
 def generate_base_grid(extent, nres):
     """Generates a 2D primitive grid.
@@ -41,21 +43,22 @@ def generate_base_grid(extent, nres):
 
     # Preprocess arguemnts.
     extent = np.array(extent)
-    
+
     if extent.ravel().size == 2:
         extent = np.array([extent, extent])
     elif extent.ravel().size == 4:
         extent = extent
     else:
         raise ValueError("extent must be either (2,2) or (2,) array")
-    
+
     if isinstance(nres, int):
         nres = (nres, nres)
-    
+
     # Prepare output and for loop.
     grid = _generate_base_grid(extent, nres)
 
     return grid
+
 
 if __name__ == '__main__':
     grid0 = generate_base_grid([0, 1], 64)
--- vortrace/vortrace.py
+++ vortrace/vortrace.py
@@ -1,38 +1,45 @@
 import Cvortrace
 import numpy as np
 
+
 class projection_cloud(object):
+
     def __init__(self, pos, dens, boundbox=None):
         pos = np.array(pos)
         dens = np.array(dens)
 
         if boundbox is None:
-            boundbox = [pos[:,0].min(), pos[:,0].max(),
-                        pos[:,1].min(), pos[:,1].max(),
-                        pos[:,2].min(), pos[:,2].max()]
+            boundbox = [
+                pos[:, 0].min(), pos[:, 0].max(), pos[:, 1].min(),
+                pos[:, 1].max(), pos[:, 2].min(), pos[:, 2].max()
+            ]
 
         self.boundbox = boundbox
 
         self.cloud = Cvortrace.PointCloud()
         self.cloud.loadPoints(pos, dens, boundbox)
         self.cloud.buildTree()
-    
-    def _make_grid(self, npix, xrng, yrng, zrng):
-        pos_start = np.zeros((npix[0]*npix[1], 3))
-        pos_end = np.zeros((npix[0]*npix[1], 3))
 
-        delta_x = (xrng[1]-xrng[0])/(npix[0]-1)
-        delta_y = (yrng[1]-yrng[0])/(npix[1]-1)
-        
+    def _make_grid(self, npix, xrng, yrng, zrng):
+        pos_start = np.zeros((npix[0] * npix[1], 3))
+        pos_end = np.zeros((npix[0] * npix[1], 3))
+
+        delta_x = (xrng[1] - xrng[0]) / (npix[0] - 1)
+        delta_y = (yrng[1] - yrng[0]) / (npix[1] - 1)
+
         for i in range(npix[0]):
             for j in range(npix[1]):
-                pos_start[i*npix[0]+j][0] = pos_end[i*npix[0]+j][0] = xrng[0] + delta_x * i
-                pos_start[i*npix[0]+j][1] = pos_end[i*npix[0]+j][1] = yrng[0] + delta_y * j
-                pos_start[i*npix[0]+j][2] = zrng[0]
-                pos_end[i*npix[0]+j][2] = zrng[1]
-        
+                pos_start[i * npix[0] +
+                          j][0] = pos_end[i * npix[0] +
+                                          j][0] = xrng[0] + delta_x * i
+                pos_start[i * npix[0] +
+                          j][1] = pos_end[i * npix[0] +
+                                          j][1] = yrng[0] + delta_y * j
+                pos_start[i * npix[0] + j][2] = zrng[0]
+                pos_end[i * npix[0] + j][2] = zrng[1]
+
         return pos_start, pos_end
-    
+
     def projection(self, xrng, yrng, npix, zrng=None):
         xrng = np.array(xrng)
         yrng = np.array(yrng)
@@ -47,7 +54,6 @@ class projection_cloud(object):
             assert zrng.size == 2
         else:
             zrng = np.array([self.boundbox[4], self.boundbox[5]])
-        
 
         extent = [xrng[0], xrng[1], yrng[0], yrng[1], zrng[0], zrng[1]]
 
@@ -60,5 +66,3 @@ class projection_cloud(object):
 
         dat = np.reshape(dat, npix)
         return dat
-
-
