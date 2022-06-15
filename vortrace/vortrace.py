7a8818f78d482e3523162092f7c93d53c8ca94f6
Gus Beane <angus.beane@cfa.harvard.edu>
2022-06-15 13:17:15 -0400

    Clean up projection code, fix linting

--- vortrace/vortrace.py
+++ vortrace/vortrace.py
@@ -45,26 +45,6 @@ class ProjectionCloud:
         self._cloud.loadPoints(pos, dens, boundbox)
         self._cloud.buildTree()
 
-    def _make_grid(self, npix, xrng, yrng, zrng):
-        pos_start = np.zeros((npix[0] * npix[1], 3))
-        pos_end = np.zeros((npix[0] * npix[1], 3))
-
-        delta_x = (xrng[1] - xrng[0]) / (npix[0] - 1)
-        delta_y = (yrng[1] - yrng[0]) / (npix[1] - 1)
-
-        for i in range(npix[0]):
-            for j in range(npix[1]):
-                pos_start[i * npix[0] +
-                          j][0] = pos_end[i * npix[0] +
-                                          j][0] = xrng[0] + delta_x * i
-                pos_start[i * npix[0] +
-                          j][1] = pos_end[i * npix[0] +
-                                          j][1] = yrng[0] + delta_y * j
-                pos_start[i * npix[0] + j][2] = zrng[0]
-                pos_end[i * npix[0] + j][2] = zrng[1]
-
-        return pos_start, pos_end
-
     def projection(self,
                    extent,
                    nres,
@@ -75,20 +55,14 @@ class ProjectionCloud:
                    pitch=0.,
                    roll=0.):
 
-        s, e = self._make_grid([nres, nres], extent, extent, bounds)
-
-        print(s[0])
-
         pos_start, pos_end = gr.generate_projection_grid(extent,
-                                                     nres,
-                                                     bounds,
-                                                     center,
-                                                     proj=proj,
-                                                     yaw=yaw,
-                                                     pitch=pitch,
-                                                     roll=roll)
-        
-        print(pos_start[0][0])
+                                                         nres,
+                                                         bounds,
+                                                         center,
+                                                         proj=proj,
+                                                         yaw=yaw,
+                                                         pitch=pitch,
+                                                         roll=roll)
 
         # Flatten before feeding into backend.
         # TODO: make C backend accept arbitrary shape?
