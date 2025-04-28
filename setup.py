cf915554c389c57b5e6d981c76e79f25bcf013ce
Gus Beane <angus.beane@cfa.harvard.edu>
2025-04-28 17:53:32 -0400

    Update install scripts

--- setup.py
+++ setup.py
@@ -1,28 +1,46 @@
 from glob import glob
 from setuptools import setup
+import pybind11
+
+# Prefer pybind11’s helper if available
 try:
     from pybind11.setup_helpers import Pybind11Extension
-except:
+except ImportError:
     from setuptools import Extension as Pybind11Extension
 
 ext_modules = [
     Pybind11Extension(
-        "Cvortrace",
-        sorted(glob("src/*.cpp")),
-        include_dirs=['include'],
-        language='c++',
-        extra_compile_args=['-std=c++11', '-DTIMING_INFO']  # Sort source files for reproducibility
+        "Cvortrace",                         # name of the generated .so
+        sorted(glob("src/*.cpp")),           # all your .cpp sources
+        include_dirs=[
+            "include",                       # your existing headers
+            pybind11.get_include(),         # pybind11 headers
+        ],
+        language="c++",
+        extra_compile_args=[
+            "-std=c++11", 
+            "-DTIMING_INFO"
+        ],
     ),
 ]
 
-setup(name='vortrace',
-      version='0.1',
-      description='Fast projections through Voronoi meshes.',
-      author='Angus Beane and Matthew Smith',
-      author_email='angus.beane@cfa.harvard.edu',
-      packages=['vortrace'],
-      install_requires=['numpy', 'numba', 'pybind11'],
-      setup_requires=['pybind11'],
-      extra_requires={'dev': ['h5py', 'pylint', 'pytest'],
-                      'test': ['h5py', 'pylint', 'pytest']},
-      ext_modules=ext_modules)
+setup(
+    name="vortrace",
+    version="0.1",
+    description="Fast projections through Voronoi meshes.",
+    author="Angus Beane and Matthew Smith",
+    author_email="angus.beane@cfa.harvard.edu",
+    packages=["vortrace"],
+    install_requires=[
+        "numpy",
+        "numba",
+        "pybind11>=2.6.0",
+    ],
+    setup_requires=["pybind11"],
+    extras_require={
+        "dev": ["h5py", "pylint", "pytest"],
+        "test": ["h5py", "pylint", "pytest"],
+    },
+    ext_modules=ext_modules,
+    zip_safe=False,
+)
