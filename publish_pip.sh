99464c87baf8a3592940f91377fea9d105bd2702
Gus Beane <angus.beane@cfa.harvard.edu>
2026-06-25 10:23:24 -0400

    Update publish_pip.sh

--- publish_pip.sh
+++ publish_pip.sh
@@ -3,6 +3,12 @@
 source venv/bin/activate
 python -m pip install --upgrade build twine
 rm -rf dist/
-python -m build
+python -m build --sdist
+
+# download artifacts from build
+# gh run download RUN_ID --dir dist/
+mv dist/cibw-wheels*/*.whl dist/
+rm -r dist/cibw-wheels*
+
 python -m twine check dist/*
 python -m twine upload dist/*
